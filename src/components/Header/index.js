a7fb1f3fe829a0961949bc865a7826b57adadfb5
JuanUruloki <juampy_1@live.com>
2024-09-30 01:31:14 -0300

    fix_responsive aplicado a mejorar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,50 +1,138 @@
-import { FaPhoneAlt } from "react-icons/fa";
-import { FaEnvelope } from "react-icons/fa";
-import { FaLongArrowAltRight } from "react-icons/fa";
+"use client";
 import Link from "next/link";
+import { FaEnvelope, FaLongArrowAltRight, FaPhoneAlt } from "react-icons/fa";
+import { useState } from "react";
 
 const Header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const navbarToggleHandler = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeNavbar = () => {
+    setNavbarOpen(false);
+  };
+
   return (
-    <div className="flex flex-col w-full h-40 font-texts ">
-      <div className="flex flex-row h-16 px-48 bg-lightBlue justify-around items-center  text-white">
-        <div className="flex text-2xl ">
-          <p>Want to Connect with us?</p>
+    <div className="flex flex-col w-full h-auto font-texts relative z-10">
+      {/* Div superior con el botón de menú hamburguesa */}
+      <div className="flex flex-row h-24 2xl:h-16 px-4 bg-lightBlue justify-evenly items-center text-white 2xl:px-48">
+        <div className="flex items-center text-xs md:text-base  2xl:text-xl 3xl:text-2xl relative">
+          {/* Logo que sobresale */}
+          <div
+            className="absolute 2xl:hidden w-36 h-36 mt-16  bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: "url(/logo.png)" }}
+          />
+          <button
+            onClick={navbarToggleHandler}
+            aria-label="Mobile Menu"
+            className="flex flex-col ml-44 2xl:hidden items-center gap-1.5 p-2"
+          >
+            <div
+              className={`w-10 h-1.5 bg-white rounded-full transition-transform duration-300 ${
+                navbarOpen ? "rotate-45 translate-y-3" : ""
+              }`}
+            />
+            <div
+              className={`w-10 h-1.5 bg-white rounded-full transition-opacity duration-300 ${
+                navbarOpen ? "opacity-0" : ""
+              }`}
+            />
+            <div
+              className={`w-10 h-1.5 bg-white rounded-full transition-transform duration-300 ${
+                navbarOpen ? "-rotate-45 -translate-y-3" : ""
+              }`}
+            />
+          </button>
+          <p className="ml-8 2xl:ml-0 2xl:mr-5">Want to Connect with us?</p>
         </div>
-        <div className="flex items-center">
-          <FaEnvelope className="pr-2 text-2xl" />
-          <p className="pr-6 ">Mail us: support@insurzella.com</p>
-          <FaPhoneAlt className="pr-2 text-2xl" />
-          <p>Call us: (877) 227 0774</p>
+        <div className="flex text-xs font-textRegular 2xl:text-xl items-center justify-evenly">
+          <FaEnvelope className=" hidden lg:block pr-2 text-xs md:text-base  xl:text-xl 3xl:text-2xl" />
+          <p className="pr-6 2xl:text-xl">Mail us: support@insurzella.com</p>
+          <FaPhoneAlt className="hidden lg:block pr-2 text-xs md:text-base  xl:text-xl 2xl:text-2xl" />
+          <p className="xl:text-xl">Call us: (877) 227 0774</p>
         </div>
       </div>
-      <div className="flex h-2/3 w-full bg-white justify-around items-center">
-      <div
-              className="w-16 h-16 rounded-xl bg-contain bg-center bg-no-repeat"
-              style={{ backgroundImage: "url(/logo.png)" }}
-            />
-        <div className="flex w-1/5 justify-evenly">
-            <Link href='/' className="hover:underline hover:underline-offset-4 hover:text-midBlue">
-                Home
-            </Link>
-            <p>|</p>
-            <Link href='/' className="hover:underline hover:underline-offset-4 hover:text-midBlue">
-                Find Insurance
-            </Link>
-            <p>|</p>
-            <Link href='/' className="hover:underline hover:underline-offset-4 hover:text-midBlue">
-                Learn
-            </Link>
-            <p>|</p>
-            <Link href='/' className="hover:underline hover:underline-offset-4 hover:text-midBlue">
-                About
-            </Link>
+
+      {/* Div que no se muestra en móvil */}
+      <div className="hidden 2xl:flex h-20 w-full bg-white justify-evenly items-center flex-wrap">
+        <div
+          className="w-16 h-16 rounded-xl bg-contain bg-center bg-no-repeat"
+          style={{ backgroundImage: "url(/logo.png)" }}
+        />
+        <div className="flex w-1/2 md:w-1/3 justify-evenly">
+          <Link
+            href="/"
+            className="hover:underline hover:underline-offset-4 hover:text-midBlue"
+          >
+            Home
+          </Link>
+          <p>|</p>
+          <Link
+            href="/"
+            className="hover:underline hover:underline-offset-4 hover:text-midBlue"
+          >
+            Find Insurance
+          </Link>
+          <p>|</p>
+          <Link
+            href="/"
+            className="hover:underline hover:underline-offset-4 hover:text-midBlue"
+          >
+            Learn
+          </Link>
+          <p>|</p>
+          <Link
+            href="/"
+            className="hover:underline hover:underline-offset-4 hover:text-midBlue"
+          >
+            About
+          </Link>
         </div>
-        <button className="flex items-center justify-evenly h-14 w-40 bg-midBlue rounded-full shadow-button text-white font-texts hover:bg-lightBlue">
+        <button className="flex items-center justify-evenly h-14 w-40 bg-midBlue rounded-full shadow-button 2xl:text-base text-white font-texts hover:bg-lightBlue">
           Call Us Now
           <FaLongArrowAltRight />
         </button>
       </div>
-      <div className="h-4 w-full bg-blue-50"/>
+
+      {/* Menú desplegable en móvil */}
+      <div
+        className={`absolute flex flex-col ml-56 mt-24 w-max border-8 border-lightBlue bg-lightBlue 2xl:hidden ${
+          navbarOpen ? "block" : "hidden"
+        } bg-lightBlue`}
+      >
+        <Link
+          href="/"
+          className="py-2 px-4 hover:bg-darkBlue text-white"
+          onClick={closeNavbar}
+        >
+          Home
+        </Link>
+        <Link
+          href="/"
+          className="py-2 px-4 hover:bg-darkBlue text-white"
+          onClick={closeNavbar}
+        >
+          Find Insurance
+        </Link>
+        <Link
+          href="/"
+          className="py-2 px-4 hover:bg-darkBlue text-white"
+          onClick={closeNavbar}
+        >
+          Learn
+        </Link>
+        <Link
+          href="/"
+          className="py-2 px-4 hover:bg-darkBlue text-white"
+          onClick={closeNavbar}
+        >
+          About
+        </Link>
+      </div>
+
+      <div className="h-4 hidden 2xl:flex w-full bg-blue-50" />
     </div>
   );
 };
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,15 +1,25 @@
 const Hero = () => {
   return (
-    <div className="flex flex-row hero w-full h-[70vh] bg-base-200 bg-gray">
-      <div className="flex flex-col items-center justify-center px-32 h-full w-1/2 ">
-        <h1 className="text-6xl text-left text-lightBlue font-heroTitle mb-12">YOUR GUIDE TO QUALITY COVERAGE</h1>
-        <h3 className="text-xl text-left font-textRegular text-slate-500 mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
-        <div className="flex w-full">
-        <button className="flex items-center justify-evenly h-14 w-56 bg-cyan rounded-full text-white text-2xl font-texts">FIND PLANS</button>
+    <div className="flex flex-col md:flex-row hero w-full h-96 xl:h-[70vh] bg-base-200 bg-gray">
+      {/* Contenedor de Texto */}
+      <div className="flex flex-col items-center justify-center pl-10 xl:px-14 2xl:px-14 py-8 2xl:py-0 h-full w-full md:w-1/2">
+        <h1 className="text-xl lg:text-3xl 2xl:text-5xl text-start lg:text-left text-lightBlue font-heroTitle leading-none mb-4 lg:mb-4">
+          YOUR GUIDE TO QUALITY COVERAGE
+        </h1>
+        <h3 className="text-xs lg:text-base xl:text-xl text-start font-textRegular text-slate-500 mb-6 lg:mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </h3>
+        <div className="flex w-full justify-center lg:justify-start">
+          <button className="flex items-center justify-evenly h-8 xl:h-14 w-36 xl:w-56 bg-cyan rounded-full text-white text-base xl:text-2xl font-texts">
+            FIND PLANS
+          </button>
         </div>
       </div>
-      <div className="flex h-full w-1/2 bg-cover bg-top bg-no-repeat"
-      style={{ backgroundImage:"url('/hero-bg.jpg')"}}
+
+      {/* Contenedor de Imagen */}
+      <div
+        className="flex h-full lg:h-full w-full lg:w-1/2 bg-cover bg-left bg-no-repeat"
+        style={{ backgroundImage: "url('/hero-bg.jpg')" }}
       />
     </div>
   );
